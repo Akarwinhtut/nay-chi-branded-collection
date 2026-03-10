9eecad3e76b809ce5ce75cd3178a5116df95aac7
Akar Win Htut <akarwinhtutold@gmail.com>
2026-03-11 01:02:12 +0630

    Always show collection grid on mobile

--- apps/shop/app/services/page.tsx
+++ apps/shop/app/services/page.tsx
@@ -37,9 +37,7 @@ export default async function CollectionPage() {
         </ScrollReveal>
       </section>
 
-      <ScrollReveal direction="scale">
-        <CollectionCatalog products={products} />
-      </ScrollReveal>
+      <CollectionCatalog products={products} />
     </div>
   );
 }
